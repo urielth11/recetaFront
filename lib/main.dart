8c959a29f31671e6f3a86397fd0f740841ef3407
unknown <soporte@SOL487.SOLIMANO.LOCAL>
2021-12-04 16:35:19 -0500

    caleb

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 //import 'package:curved_navigation_bar/curved_navigation_bar.dart';
-// import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:recetaapp/login_page.dart';
+import 'package:recetaapp/services/authservice.dart';
 
 import 'menu.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+  await Firebase.initializeApp();
   runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
 }
 
@@ -22,7 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: LoginPage(),
+      home: AuthService().handleAuth(),
     );
   }
 }
--- lib/signup.dart
+++ lib/signup.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:recetaapp/reset.dart';
+import 'package:recetaapp/error_handler.dart';
 import 'package:recetaapp/services/authservice.dart';
-import 'package:recetaapp/signup.dart';
+import 'package:recetaapp/error_handler.dart';
 import 'package:recetaapp/services/authservice.dart';
-import 'package:recetaapp/signup.dart';
 
-class LoginPage extends StatefulWidget {
+class SignupPage extends StatefulWidget {
   @override
-  _LoginPageState createState() => _LoginPageState();
+  _SignupPageState createState() => _SignupPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _SignupPageState extends State<SignupPage> {
   final formKey = new GlobalKey<FormState>();
 
   late String email, password;
@@ -47,10 +46,10 @@ class _LoginPageState extends State<LoginPage> {
         body: Container(
             height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width,
-            child: Form(key: formKey, child: _buildLoginForm())));
+            child: Form(key: formKey, child: _buildSignupForm())));
   }
 
-  _buildLoginForm() {
+  _buildSignupForm() {
     return Padding(
         padding: const EdgeInsets.only(left: 25.0, right: 25.0),
         child: ListView(children: [
@@ -60,16 +59,12 @@ class _LoginPageState extends State<LoginPage> {
               width: 200.0,
               child: Stack(
                 children: [
-                  Text('Hello',
+                  Text('Signup',
                       style: TextStyle(fontFamily: 'Trueno', fontSize: 60.0)),
+                  //Dot placement
                   Positioned(
-                      top: 50.0,
-                      child: Text('There',
-                          style:
-                              TextStyle(fontFamily: 'Trueno', fontSize: 60.0))),
-                  Positioned(
-                      top: 97.0,
-                      left: 175.0,
+                      top: 62.0,
+                      left: 200.0,
                       child: Container(
                           height: 10.0,
                           width: 10.0,
@@ -108,27 +103,16 @@ class _LoginPageState extends State<LoginPage> {
                 this.password = value;
               },
               validator: (value) =>
-                  value!.isEmpty ? 'Password es requerido' : null),
-          SizedBox(height: 5.0),
-          GestureDetector(
-              onTap: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => ResetPassword()));
-              },
-              child: Container(
-                  alignment: Alignment(1.0, 0.0),
-                  padding: EdgeInsets.only(top: 15.0, left: 20.0),
-                  child: InkWell(
-                      child: Text('Olvido su contraseña',
-                          style: TextStyle(
-                              color: greenColor,
-                              fontFamily: 'Trueno',
-                              fontSize: 11.0,
-                              decoration: TextDecoration.underline))))),
+                  value!.isEmpty ? 'Password is required' : null),
           SizedBox(height: 50.0),
           GestureDetector(
             onTap: () {
-              if (checkFields()) AuthService().signIn(email, password, context);
+              if (checkFields())
+                AuthService().signUp(email, password).then((userCreds) {
+                  Navigator.of(context).pop();
+                }).catchError((e) {
+                  ErrorHandler().errorDialog(context, e);
+                });
             },
             child: Container(
                 height: 50.0,
@@ -138,47 +122,17 @@ class _LoginPageState extends State<LoginPage> {
                     color: greenColor,
                     elevation: 7.0,
                     child: Center(
-                        child: Text('LOGIN',
+                        child: Text('SIGN UP',
                             style: TextStyle(
                                 color: Colors.white, fontFamily: 'Trueno'))))),
           ),
           SizedBox(height: 20.0),
-          GestureDetector(
-            onTap: () {
-              AuthService().fbSignIn();
-            },
-            child: Container(
-                height: 50.0,
-                color: Colors.transparent,
-                child: Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                          color: Colors.black,
-                          style: BorderStyle.solid,
-                          width: 1.0),
-                      color: Colors.transparent,
-                      borderRadius: BorderRadius.circular(25.0)),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Center(
-                          child: ImageIcon(AssetImage('assets/facebook.png'),
-                              size: 15.0)),
-                      SizedBox(width: 10.0),
-                      Center(
-                          child: Text('Ingrese con Facebook',
-                              style: TextStyle(fontFamily: 'Trueno'))),
-                    ],
-                  ),
-                )),
-          ),
-          SizedBox(height: 25.0),
           Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            Text('Nuevo en MiApp Recetas ?'),
-            SizedBox(width: 5.0),
             InkWell(
-                onTap: () {},
-                child: Text('Register',
+                onTap: () {
+                  Navigator.of(context).pop();
+                },
+                child: Text('Go back',
                     style: TextStyle(
                         color: greenColor,
                         fontFamily: 'Trueno',
